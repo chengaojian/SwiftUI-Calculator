87faa1785fe989629de020e1cede1fab4912d96b
chengaojian <chengaojian@haixun360.com>
2019-08-29 10:39:18 +0800

    小专栏第3章:Hello-SwiftUI完整Demo

---
+++ Calculation/Calculation/ContentView.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  Calculation
+//
+//  Created by CH on 2019/8/17.
+//  Copyright © 2019 sanhai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack (spacing: 12) {
+            Spacer()
+            Text("0")
+                .font(.system(size: 76))
+                .minimumScaleFactor(0.5)
+                .padding(.trailing, 24)
+                .lineLimit(1)
+                .frame(minWidth: 0, maxWidth: .infinity,alignment: .trailing)
+            CalculatorButtonPad()
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+            ContentView()
+        }
+}
+#endif
+
+
+struct CalculatorButtonPad: View {
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip),
+         .command(.percent), .op(.divide)],
+        [.digit(7), .digit(8), .digit(9), .op(.multiply)],
+        [.digit(4), .digit(5), .digit(6), .op(.minus)],
+        [.digit(1), .digit(2), .digit(3), .op(.plus)],
+        [.digit(0), .dot, .op(.equal)]
+    ]
+    
+    var body: some View {
+        
+        VStack (spacing: 8) {
+            ForEach(pad, id: \.self) { row in
+                CalculatorButtonRow(row: row)
+            }
+        }
+    }
+}
+
+
+struct CalculatorButtonRow: View {
+    let row: [CalculatorButtonItem]
+    var body: some View {
+       HStack {
+            ForEach(row, id: \.self) { item in
+                CalculatorButton(
+                    title: item.title,
+                    size: item.size,
+                    backgroundColorName: item.backgroundColorName)
+                {
+                        print("Button: \(item.title)")
+                }
+            }
+       }
+    }
+}
+
+
+struct CalculatorButton: View {
+    let fontsize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action:() -> Void
+    
+    var body: some View {
+        Button.init(action: action) {
+            Text(title)
+                .font(.system(size: fontsize))
+                .foregroundColor(.white)
+                .frame(width: size.width, height: size.height, alignment: .center)
+                .background(Color(backgroundColorName))
+                .cornerRadius(size.width / 2)
+            
+        }
+    }
+}
